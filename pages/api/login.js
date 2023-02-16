63d64b79f8f7a64aabdcb91753f0f266c14e2de2
Stanley-Arthur <107648515+Stanley-Arthur@users.noreply.github.com>
2023-02-16 00:15:29 +0000

    Update login.js

--- pages/api/login.js
+++ pages/api/login.js
@@ -10,7 +10,7 @@ const handler = (req, res) =>{
     ) {
       res.setHeader(
         "Set-Cookie",
-        cookie.serialize("token", process.env.TOKEN, {
+        cookie.serialize("token", process.env.TOKEN_KEY, {
           maxAge: 60 * 60,
           sameSite: "strict",
           path: "/",
